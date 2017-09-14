90a3d058a59626328996e57275971fa6e09cbc45
Shubham Suri <shubham013@live.com>
2017-09-14 15:04:37 +0530

    partial - lvl7

--- contacts/src/app/app.module.ts
+++ contacts/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ContactDeatilsComponent } from './contact-deatils/contact-deatils.component';
@@ -9,6 +10,9 @@ import { ContactsComponent } from './contacts.component';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppRoutingModule } from './app-routing.module';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -20,8 +24,9 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
-
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   providers: [ContactInfoService],
   bootstrap: [AppComponent]
--- contacts/src/app/contact-info.service.ts
+++ contacts/src/app/contact-info.service.ts
@@ -1,35 +1,57 @@
-import { Injectable } from '@angular/core';
-import { Contact } from './Contact';
+// import { Injectable } from '@angular/core';
+// import { Contact } from './Contact';
+//
+// const CONTACTS: Contact[] = [{
+//   name: 'Shubham',
+//   number: '9041908421',
+//   id: '1'
+// },
+// {
+//   name: 'Shubha',
+//   number: '9041908421',
+//   id: '2'
+// },
+// {
+//   name: 'Shubh',
+//   number: '9041908421',
+//   id: '3'
+// }
+// ];
+//
+// @Injectable()
+// export class ContactInfoService {
+//
+//   constructor() {
+//    }
+//
+//    getContacts(): Promise<Contact[]> {
+//      return Promise.resolve(CONTACTS);
+//    }
+//
+//    getContact(id: string): Promise<Contact> {
+//     return this.getContacts()
+//                .then(heroes => heroes.find(hero => hero.id === id));
+//   }
+// }
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
 
-const CONTACTS: Contact[] = [{
-  name: 'Shubham',
-  number: '9041908421',
-  id: '1'
-},
-{
-  name: 'Shubha',
-  number: '9041908421',
-  id: '2'
-},
-{
-  name: 'Shubh',
-  number: '9041908421',
-  id: '3'
+import 'rxjs/add/operator/toPromise';
+
+import { Contact } from './contact';
+
+private contactsUrl = 'api/contacts';  // URL to web api
+
+constructor(private http: Http) { }
+
+getHeroes(): Promise<Hero[]> {
+  return this.http.get(this.heroesUrl)
+             .toPromise()
+             .then(response => response.json().data as Hero[])
+             .catch(this.handleError);
 }
-];
 
-@Injectable()
-export class ContactInfoService {
-
-  constructor() {
-   }
-
-   getContacts(): Promise<Contact[]> {
-     return Promise.resolve(CONTACTS);
-   }
-
-   getContact(id: string): Promise<Contact> {
-    return this.getContacts()
-               .then(heroes => heroes.find(hero => hero.id === id));
-  }
+private handleError(error: any): Promise<any> {
+  console.error('An error occurred', error); // for demo purposes only
+  return Promise.reject(error.message || error);
 }
