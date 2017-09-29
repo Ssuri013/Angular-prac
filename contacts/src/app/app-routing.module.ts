2501018f0890488fd195b69d0e676b6869fcc989
Shubham Suri <shubham013@live.com>
2017-09-29 18:12:17 +0530

    hmmm

--- contacts/src/app/app-routing.module.ts
+++ contacts/src/app/app-routing.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ContactDeatilsComponent } from './contact-deatils/contact-deatils.component';
 import { ContactsComponent } from './contacts.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
